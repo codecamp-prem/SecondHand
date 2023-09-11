4ddbd45ff029b627ea7cf25600a48aab88df864a
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-11 19:31:18 +0545

    handling long text

--- app/components/Card.js
+++ app/components/Card.js
@@ -8,8 +8,12 @@ function Card({ title, subtitle, imageSrc }) {
     <View style={styles.card}>
       <Image style={styles.image} source={imageSrc} />
       <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subtitle}>{subtitle}</AppText>
+        <AppText style={styles.title} numberOfLines={1}>
+          {title}
+        </AppText>
+        <AppText style={styles.subtitle} numberOfLines={2}>
+          {subtitle}
+        </AppText>
       </View>
     </View>
   );
